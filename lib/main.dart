178ae7372e4af44ac5fa44f6303c4fb7a9b73990
NetherVikings <jeremiahhay@hotmail.com>
2021-09-08 19:49:02 -0700

    just doing some testing and making an uber like UI

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+void main() => runApp(Start());
+
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [buildMap(), buildFloatingSearchBar(context), bottomSheet()],
+      ),
+    );
+  }
+
+  Widget buildFloatingSearchBar(BuildContext context) {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {},
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildMap() {
+    return GoogleMap(
+        zoomControlsEnabled: false,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ));
+  }
+
+  Widget bottomSheet() {
+    return DraggableScrollableSheet(
+      builder: (context, scrollController) => Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          child: SingleChildScrollView(
+            child: Row(
+              children: const <Widget>[
+                Expanded(
+                  child: Text('Deliver features faster',
+                      textAlign: TextAlign.center),
+                ),
+                Expanded(
+                  child:
+                      Text('Craft beautiful UIs', textAlign: TextAlign.center),
+                ),
+                Expanded(
+                  child: FittedBox(
+                    fit: BoxFit.contain, // otherwise the logo will be tiny
+                    child: FlutterLogo(),
+                  ),
+                ),
+              ],
+            ),
+          )),
+      initialChildSize: 0.1,
+      minChildSize: 0.1,
+      maxChildSize: 0.4,
+    );
+  }
+}
